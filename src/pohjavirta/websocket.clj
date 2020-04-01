b7cf5f53d59e09b9c89645b311eecbacb4c05237
Nikola Peric <nik@nikperic.com>
2020-03-31 23:18:48 -0400

    Improve ws config, remove conditional

--- src/pohjavirta/websocket.clj
+++ src/pohjavirta/websocket.clj
@@ -4,31 +4,37 @@
                                         AbstractReceiveListener
                                         BufferedTextMessage]
            [io.undertow.websockets.spi WebSocketHttpExchange]
-           [io.undertow Handlers]))
+           [io.undertow Handlers]
+           [org.xnio ChannelListener]))
 
 ;; this may fit better elsewhere. At first start here though to keep modular
 
-(defn ws-listener [{:keys [on-receive on-close on-error]}]
+(defn ws-listener
+  "Default listener"
+  [{:keys [on-receive on-close on-error]
+    :or   {on-receive (constantly nil)
+           on-close   (constantly nil)
+           on-error   (constantly nil)}}]
   (proxy [AbstractReceiveListener] []
     (onFullTextMessage [^WebSocketChannel channel ^BufferedTextMessage message]
-      (when on-receive
-        (on-receive {:channel channel
-                     :data (.getData message)})))
+      (on-receive {:channel channel
+                   :data    (.getData message)}))
     (onClose [^WebSocketChannel ws-channel _]
-      (when on-close
-        (on-close {:channel ws-channel})))
+      (on-close {:channel ws-channel}))
     (onError [^WebSocketChannel channel ^Throwable error]
-      (when on-error
-        (on-error {:channel channel
-                   :error   error})))))
+      (on-error {:channel channel
+                 :error   error}))))
 
 (defn ws-callback
-  [{:keys [on-connect] :as ws-opts}]
-  (reify WebSocketConnectionCallback
-    (^void onConnect [_ ^WebSocketHttpExchange exchange ^WebSocketChannel channel]
-      (.set (.getReceiveSetter channel) (ws-listener ws-opts))
-      (on-connect {:channel channel})
-      (.resumeReceives channel))))
+  [{:keys [on-connect listener] :as ws-opts}]
+  (let [listener (if (instance? ChannelListener listener)
+                   listener
+                   (ws-listener ws-opts))]
+    (reify WebSocketConnectionCallback
+      (^void onConnect [_ ^WebSocketHttpExchange exchange ^WebSocketChannel channel]
+        (.set (.getReceiveSetter channel) listener)
+        (on-connect {:channel channel})
+        (.resumeReceives channel)))))
 
 (defn ws-handler
   "Convenience function to create a basic websocket handler"
